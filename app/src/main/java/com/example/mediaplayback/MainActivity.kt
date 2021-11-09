ae95d563f8db731eecc8f5313a4658d9337c2d5b
oskarlasota <frezzyysmite@gmail.com>
2021-11-09 19:29:07 +0000

    setting listeners for selecting media video

--- app/src/main/java/com/example/mediaplayback/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayback/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.mediaplayback
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val OBTAIN_VIDEO_REQUEST = 4
+    }
+
+    private val registerResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        when(result.resultCode){
+            RESULT_OK -> ""
+            RESULT_CANCELED ->""
+            RESULT_FIRST_USER -> ""
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
+
+    }
+
+    private fun setListeners(){
+        this.findViewById<Button>(R.id.btn_upload).setOnClickListener {
+            //intent should be provided by viewmodel
+            val intent = Intent()
+            intent.type = "video/*"
+            intent.action = Intent.ACTION_PICK
+            registerResult.launch(intent)
+        }
     }
 }
