a1dfd900270aff53f7a45a0570ad24809c409e26
oskarlasota <frezzyysmite@gmail.com>
2021-11-09 20:14:07 +0000

    creating a videoviewer and playing selected video from gallery

--- app/src/main/java/com/example/mediaplayback/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayback/MainActivity.kt
@@ -4,17 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.VideoView
 import androidx.activity.result.contract.ActivityResultContracts
+import java.io.File
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        val OBTAIN_VIDEO_REQUEST = 4
-    }
+    private lateinit var videoPlayer : VideoView
 
     private val registerResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
         when(result.resultCode){
-            RESULT_OK -> ""
+            RESULT_OK -> {
+                val videoURI = result.data?.data
+                videoPlayer.setVideoURI(videoURI)
+                videoPlayer.start()
+            }
             RESULT_CANCELED ->""
             RESULT_FIRST_USER -> ""
         }
@@ -23,7 +28,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        videoPlayer = findViewById(R.id.videoplayer)
         setListeners()
 
     }
