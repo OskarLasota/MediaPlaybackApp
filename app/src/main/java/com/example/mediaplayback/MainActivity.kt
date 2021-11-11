cb7980ecc4814bcad28fc476982e8a5cf8343ae6
oskarlasota <frezzyysmite@gmail.com>
2021-11-11 01:35:21 +0000

    chore: playing a video from a URL

--- app/src/main/java/com/example/mediaplayback/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayback/MainActivity.kt
@@ -1,29 +1,31 @@
 package com.example.mediaplayback
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.MediaController
 import android.widget.VideoView
 import androidx.activity.result.contract.ActivityResultContracts
-import java.io.File
-import java.net.URI
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var videoPlayer : VideoView
+    private lateinit var videoPlayer: VideoView
+    private val vid_url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
 
-    private val registerResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-        when(result.resultCode){
-            RESULT_OK -> {
-                val videoURI = result.data?.data
-                videoPlayer.setVideoURI(videoURI)
-                videoPlayer.start()
+    private val registerResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            when (result.resultCode) {
+                RESULT_OK -> {
+                    val videoURI = result.data?.data
+                    videoPlayer.setVideoURI(videoURI)
+                    videoPlayer.start()
+                }
+                RESULT_CANCELED -> ""
+                RESULT_FIRST_USER -> ""
             }
-            RESULT_CANCELED ->""
-            RESULT_FIRST_USER -> ""
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,13 +35,32 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun setListeners(){
-        this.findViewById<Button>(R.id.btn_upload).setOnClickListener {
+    private fun setListeners() {
+        this.findViewById<Button>(R.id.btn_select).setOnClickListener {
             //intent should be provided by viewmodel
             val intent = Intent()
             intent.type = "video/*"
             intent.action = Intent.ACTION_PICK
             registerResult.launch(intent)
+        }
+        this.findViewById<Button>(R.id.btn_upload).setOnClickListener {
+            //send to backend server
+        }
+        this.findViewById<Button>(R.id.btn_stream).setOnClickListener{
+            try {
+                val mediacontroller = MediaController(this)
+                videoPlayer.setMediaController(mediacontroller)
+                videoPlayer.setVideoPath(vid_url)
+                videoPlayer.start()
+            }catch (e : Exception){
+                e
+            }
+            videoPlayer.setOnErrorListener { mediaPlayer, i, i2 ->
+                true
+            }
+            videoPlayer.setOnCompletionListener {
+                println("completed")
+            }
         }
     }
 }
